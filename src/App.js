c8e0c5175287d1cac5916402bb375670a4801307
wcywin <wojciech.cywinski@qualent.pl>
2020-05-10 23:13:21 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Styled Componennts v5
         </a>
       </header>
     </div>
