1861a467c27ed35afd9802d107b52f52ecc3d976
wcywin <wcywin@gmail.com>
2020-05-11 22:21:32 +0200

    styling react components and extending existing styled components + workaround for Link of react-router-dom using props

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import { Link as ReactRouterDomLink } from 'react-router-dom'
 
 const HeaderWrapper = styled.header`
 	height: 60px;
@@ -13,15 +13,46 @@ const HeaderWrapper = styled.header`
 	background: #eee;
 `
 
+const Menu = styled.nav`
+	display: flex;
+	position: relative;
+	width: initial;
+	border-bottom: none;
+	margin: auto 0 auto auto;
+	font-family: 'Open Sans', sans-serif;
+	background: none;
+	left: initial;
+	top: initial;
+`
+
+const Link = ({ isActive, children, ...props }) => {
+	return (
+		<ReactRouterDomLink {...props} >
+			{ children }
+		</ReactRouterDomLink>
+	)
+}
+
+const StyledLink = styled(Link)`
+	padding: 4px 8px;
+	display: block;
+	text-align: center;
+	box-sizing: border-box;
+	margin: auto 0;
+	font-weight: ${p => p.isActive ? 'bold' : 'normal'};
+`
+
 export function Header() {
 	return (
 		<HeaderWrapper>
-			<Link to="/">
-				Home
-			</Link>
-			<Link to="/login">
-				Login
-			</Link>
+			<Menu>
+				<StyledLink to="/">
+					Home
+				</StyledLink>
+				<StyledLink to="/login" isActive>
+					Login
+				</StyledLink>
+			</Menu>
 		</HeaderWrapper>
 	)
 }
