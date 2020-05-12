ab2dfd247bf59c77f4408a50da8eb24defa50e8f
wcywin <wcywin@gmail.com>
2020-05-12 08:44:48 +0200

    add Input styled component and use in Login page

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,10 +1,50 @@
-import React from 'react'
-import { PageLayout } from 'components/common'
+import React, { useState } from 'react'
+import { PageLayout, Input } from 'components/common'
+import styled from 'styled-components'
+
+const Form = styled.form`
+	width: 100%;
+	max-width: 400px;
+	background: white;
+	border: 1px solid #eee;
+	padding: 16px;
+	box-sizing: border-box;
+	color: black;
+	border-radius: 4px;
+`
 
 export default function Login() {
+	const [formFields, setFormFields] = useState({ username: '', password: '' })
+
+	function handleInputChange(e) {
+		e.persist();
+		setFormFields(s => ({
+			...s,
+			[e.target.name]: e.target.value
+		}))
+	}
+
 	return (
 		<PageLayout>
-			Login
+			<h1>
+				Login
+			</h1>
+			<Form>
+				<Input
+					value={formFields.username}
+					name="username"
+					placeholder="Username"
+					type="text"
+					onChange={handleInputChange}
+				/>
+				<Input
+					value={formFields.password}
+					name="password"
+					placeholder="Password"
+					type="password"
+					onChange={handleInputChange}
+				/>
+			</Form>
 		</PageLayout>
 	)
 }
